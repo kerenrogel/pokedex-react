cb77f12d9feb11044bcf034e04b23187f1004cc8
Keren Rogel <kerenrogel@gmail.com>
2019-05-17 07:36:32 +0800

    Created list and card layout

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,14 @@
+import React, {  Component }  from 'react';
+import '../styles/styles.css';
+
+class PokemonCard extends Component {
+  render() {
+    return(
+      <section class="poke-card">
+      	<p>PokeCard</p>
+      </section>
+    );
+  }
+}
+
+export default PokemonCard;
---
+++ src/components/PokemonList.js
@@ -0,0 +1,14 @@
+import React, {  Component }  from 'react';
+import '../styles/styles.css';
+
+class PokemonList extends Component {
+  render() {
+    return(
+      <section class="poke-list">
+      	<p>Pokelist</p>
+      </section>
+    );
+  }
+}
+
+export default PokemonList;
